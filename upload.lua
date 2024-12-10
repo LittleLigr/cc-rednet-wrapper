eab0c6c9e6cd221571917f6bc16ff7a8a47ea155
Shadrin Igor <igor@shadrins.ru>
2024-12-11 00:42:58 +0300

    i

---
+++ upload.lua
@@ -0,0 +1,9 @@
+rm rpc_client.lua
+rm rpc_host.lua
+rm rpc_common.lua
+rm rednet-wrapper.lua
+
+wget https://raw.githubusercontent.com/LittleLigr/cc-rednet-wrapper/refs/heads/main/rednet-wrapper.lua
+wget https://raw.githubusercontent.com/LittleLigr/cc-rednet-wrapper/refs/heads/main/rpc_common.lua
+wget https://raw.githubusercontent.com/LittleLigr/cc-rednet-wrapper/refs/heads/main/rpc_host.lua
+wget https://raw.githubusercontent.com/LittleLigr/cc-rednet-wrapper/refs/heads/main/rpc_client.lua
