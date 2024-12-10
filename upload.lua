5732b39e656e9002beeea1f95c42173c0412d3d0
Shadrin Igor <igor@shadrins.ru>
2024-12-11 00:44:15 +0300

    i

--- upload.lua
+++ upload.lua
@@ -1,9 +1,9 @@
-rm rpc_client.lua
-rm rpc_host.lua
-rm rpc_common.lua
-rm rednet-wrapper.lua
+shell.execute("rm rpc_client.lua")
+shell.execute("rm rpc_host.lua")
+shell.execute("rm rpc_common.lua")
+shell.execute("rm rednet-wrapper.lua")
 
-wget https://raw.githubusercontent.com/LittleLigr/cc-rednet-wrapper/refs/heads/main/rednet-wrapper.lua
-wget https://raw.githubusercontent.com/LittleLigr/cc-rednet-wrapper/refs/heads/main/rpc_common.lua
-wget https://raw.githubusercontent.com/LittleLigr/cc-rednet-wrapper/refs/heads/main/rpc_host.lua
-wget https://raw.githubusercontent.com/LittleLigr/cc-rednet-wrapper/refs/heads/main/rpc_client.lua
+shell.execute("wget https://raw.githubusercontent.com/LittleLigr/cc-rednet-wrapper/refs/heads/main/rednet-wrapper.lua")
+shell.execute("wget https://raw.githubusercontent.com/LittleLigr/cc-rednet-wrapper/refs/heads/main/rpc_common.lua")
+shell.execute("wget https://raw.githubusercontent.com/LittleLigr/cc-rednet-wrapper/refs/heads/main/rpc_host.lua")
+shell.execute("wget https://raw.githubusercontent.com/LittleLigr/cc-rednet-wrapper/refs/heads/main/rpc_client.lua")
