c32f8c07aa09bf954edd837ed20615e09d0c03d4
Shadrin Igor <igor@shadrins.ru>
2024-12-11 00:04:42 +0300

    i

---
+++ rednet-wrapper.lua
@@ -0,0 +1,49 @@
+local Wrapper = {}
+Wrapper.rednet = {}
+Wrapper.rpc_call = {}
+Wrapper.regs = {}
+
+function get_func_args_count(func)
+  return debug.getinfo(func).nparams;
+end
+
+
+function Wrapper:host(protocol, name)
+  Wrapper.rednet.protocol = protocol
+  Wrapper.rednet.name = name
+  return Wrapper.rpc_call
+end
+
+
+function Wrapper:client(protocol, name)
+  Wrapper.rednet.protocol = protocol
+  Wrapper.rednet.name = name
+  rednet.host(protocol, name)
+  return Wrapper.regs
+end
+
+
+function call_reg_func(name, ...)
+  if Wrapper.regs[name].params_count == select('#', ...) then
+    local message = {}
+    message.rpc = {}
+    message.rpc[name] = {...}
+    rednet.broadcast(message, Wrapper.rednet.protocol)
+  end 
+end
+
+
+function Wrapper:register(name, func)
+  Wrapper.regs[name] = {}
+  Wrapper.regs[name].params_count = get_func_args_count(func)
+  Wrapper.regs[name].call = func
+  Wrapper.rpc_call[name] = (
+    function (...) 
+      call_reg_func(name, ...)
+    end
+  )
+  print("New wrapper registered="..name)
+end
+
+
+return Wrapper
